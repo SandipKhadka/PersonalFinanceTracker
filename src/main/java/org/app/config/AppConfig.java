7fa1642b921dbf361d0d63096402c57e03fbbf8a
sandip <sandip29bit2021@kcc.edu.np>
2024-02-26 18:36:47 +0545

    this is doen

--- src/main/java/org/app/config/AppConfig.java
+++ src/main/java/org/app/config/AppConfig.java
@@ -1,7 +1,6 @@
 package org.app.config;
 
 import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -22,6 +21,7 @@ public class AppConfig {
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        @SuppressWarnings("null")
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         return jdbcTemplate;
     }

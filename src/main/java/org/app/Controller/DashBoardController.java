ad9c94dfaa3477cf5abda09951007837982c57dc
sandip <sandip28bit2021@kcc.edu.np>
2024-05-13 18:55:45 +0545

    added income in home page

--- src/main/java/org/app/Controller/DashBoardController.java
+++ src/main/java/org/app/Controller/DashBoardController.java
@@ -1,12 +1,27 @@
 package org.app.Controller;
 
+import org.app.service.ExpensenService;
+import org.app.service.IncomeService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DashBoardController {
-    @GetMapping("/dashboard")
-    public String dhshBoard() {
-        return "user_dashboard";
+    IncomeService incomeService;
+    ExpensenService expensenService;
+
+    public DashBoardController(IncomeService incomeService, ExpensenService expensenService) {
+        this.incomeService = incomeService;
+        this.expensenService = expensenService;
+    }
+
+
+    @RequestMapping("/dashboard")
+    public ModelAndView showIncome() {
+        ModelAndView modelAndView = new ModelAndView("user_dashboard");
+        int income = incomeService.getIncomeAmount();
+        modelAndView.addObject("income", income);
+        return modelAndView;
     }
 }
